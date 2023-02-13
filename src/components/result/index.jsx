8c4cc3dccc57848093021d3a1191b9d68f0e1718
richforest0506 <richforest0506@gmail.com>
2023-02-13 15:41:34 -0500

    result page css added

--- src/components/result/index.jsx
+++ src/components/result/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import response from './response'
+import './result.css'
 
 const Result = () => {
-  return <div>
-    <div>Your Result</div>
-    <div>{response[0].title}</div>
-    <div>{response[0].description}</div>
+  return <div className='result'>
+    <div className='result-header'>Your Result</div>
+    <div className='result-title'>{response[0].title}</div>
+    <div className='result-content'>{response[0].description}</div>
   </div>
 }
 
